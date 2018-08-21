a964b901932905844a506c9f488ddb2ae086136c
Andrew Pisani <bondichi@gmail.com>
2018-08-21 13:29:20 +1000

    added survey models and seeds plus controller for survey responses

---
+++ test/controllers/survey_responses_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class SurveyResponsesControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get survey_responses_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get survey_responses_new_url
+    assert_response :success
+  end
+
+end
