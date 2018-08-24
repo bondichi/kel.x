bd01d85e9a1659e7909b9d7f7ac669528412487e
Andrew Pisani <bondichi@gmail.com>
2018-08-24 15:07:04 +1000

    survey prfile association

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -4,7 +4,7 @@ class SurveyResponsesController < ApplicationController
 
   def new
   	@survey = Survey.find(params[:survey_id])
-  	@survey_response = SurveyResponse.find_or_create_by!(survey: @survey)
+  	@survey_response = SurveyResponse.find_or_create_by!(survey: @survey, profile: current_user.profile)
   	redirect_to products_path  if @survey_response.unanswered_questions.empty?
   end
 end
