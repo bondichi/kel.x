beb7793cafacbcc47cd067976b26e60a3f189775
Andrew Pisani <bondichi@gmail.com>
2018-08-28 16:20:58 +1000

    fixed assets in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,7 +69,7 @@ q1 = Question.new(content: {type: "selection", question: "Which of these brands 
 q2 = Question.new(content: {type: "selection", question: "Which size would you usually wear in your chosen brand?", option_type: 'text', options: [["XS", 1], ["S", 2], ["M", 3], ["L", 4], ["XL", 5], ["XL", 6], ["XXL", 7], ["XXXL", 8], ["4XL", 9], ["5XL", 10]] })
 q3 = Question.new(content: {type: "selection", question: "Which fit do you usually like to wear in a shirt?", option_type: 'image', options: [["slim.png", 1], ["tailored.png", 2], ["standard.png", 3]] })
 q4 = Question.new(content: {type: "selection", question: "Which style to do prefer?", option_type: 'image', options: [["smart.jpg", 1], ["smart-casual.jpg", 2], ["casual.png", 3]] })
-q5 = Question.new(content: {type: "selection", question: "How would you categorise your skin tone?", option_type: 'image', options: [["tone1", 1], ["tone2", 2], ["tone3", 3], ["tone4", 4], ["tone5", 5], ["tone6", 6]] })
+q5 = Question.new(content: {type: "selection", question: "How would you categorise your skin tone?", option_type: 'image', options: [["tone1.png", 1], ["tone2.png", 2], ["tone3.png", 3], ["tone4.png", 4], ["tone5.png", 5], ["tone6.png", 6]] })
 q6 = Question.new(content: {type: "selection", question: "Are there any issues you usually have with shirts?", option_type: 'text', options: [["Shirt collar too tight", 1], ["Shirt collar too loose", 2], ["Shirt length too long", 3], ["Shirt length too short", 4], ["Shirt sleeve too long", 5], ["Shirt sleeves too short", 6], ["Shirt sleeves too tight", 7], ["Shirt body too loose", 8], ["Shirt body too tight", 9], ["No Issues", 10]] })
 q7 = Question.new(
   content: {
